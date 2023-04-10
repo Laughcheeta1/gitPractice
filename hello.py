daf3e62a53141c6746aefa9c0e4b1c7b5003a6ff
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2023-04-09 20:36:46 -0500

    added some security

--- hello.py
+++ hello.py
@@ -1,9 +1,5 @@
 def main():
     print("hello, we are going to sum two numbers")
-    num1 = int(input("enter first number: "))
-    num2 = int(input("enter second number: "))
-
-    print(f"The sum is {num1 + num2}")
 
 
 if __name__ == "__main__":
